8eadedf06a6007290b8db119e67de05bfda8ad23
Cora Zhu <zhiyi.zhu@lincolnuni.ac.nz>
2023-12-06 15:11:52 +1300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
-  purge: ["./public/**/*.html"],
+  content: ["./public/**/*.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+      ptserif: ['PT Serif', 'serif'],
+    },
+    colors: {
+      'grey': '#E4E4E4',
+      'white': '#FFFFFF',
+      'light-grey':'#F5F5F5',
+      'black': '#000000',
+      'green':'#009E73',
+      'dark-blue':'#0072B2',
+      'light-blue':'#56B4E9',
+      'yellow':'#F0E442',
+      'amber':'#E69F00',
+      'orange':'#D96E1A',
+      'pink':'#CC79A7'
+      
+    },
+    extend: {
+      
+    },
   },
   variants: {},
   plugins: [],
